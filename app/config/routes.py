88b052fb8a56a6d5535c74738ab8b9ae1cfd0cc7
Ketul Patel <kjpatel2010@gmail.com>
2015-08-25 16:50:59 -0700

    finished auto-routes

--- app/config/routes.py
+++ app/config/routes.py
@@ -10,6 +10,11 @@ from system.core.router import routes
     
     Start by defining the default controller
     Pylot will look for the index method in the default controller to handle the base route
+
+    Pylot will also automatically generate routes that resemble: '/controller/method/parameters'
+    For example if you had a products controller with an add method that took one parameter 
+    named id the automatically generated url would be '/products/add/<id>'
+    The automatically generated routes respond to all of the http verbs (GET, POST, PUT, PATCH, DELETE)
 """
 routes['default_controller'] = 'Welcome'
 """
--- system/init/routes.py
+++ system/init/routes.py
@@ -6,25 +6,48 @@
     Supports 5 http verbs (GET, POST, PUT, PATCH, DELETE)
 """
 import importlib
+import inspect
+from types import FunctionType
 from app.config.routes import routes
+from app.controllers import controllers_list
 
 def _set_route(app, route_name, pattern, controller, action, **kwargs):
     app.add_url_rule(pattern, view_func=controller.as_view(route_name, action), **kwargs)
 
+_routed_methods = []
+_verbs = ('GET', 'POST', 'PUT', 'PATCH', 'DELETE')
+
 def initialize_routes(app):
-    verbs = ('GET', 'POST', 'PUT', 'PATCH', 'DELETE')
+    """ Set up routes based on routing rules outlined in routes config file """
     for key in routes:
+        """ sets up default controller, and all routes listed on the routes dictionary """
         if key == 'default_controller':
             controller = getattr(importlib.import_module('app.controllers.'+routes['default_controller']), routes['default_controller'])
             _set_route(app, 'default_index', '/', controller, 'index')
+            _routed_methods.append(routes['default_controller'] + '#index')
         else:
-            if key in verbs:
+            if key in _verbs:
                 for route in routes[key]:
                     route_handler = routes[key][route].split("#", 1)
                     controller = getattr(importlib.import_module('app.controllers.'+route_handler[0]), route_handler[0])
                     _set_route(app, routes[key][route], route, controller, route_handler[1], methods=[key])
+                    _routed_methods.append(routes[key][route])
             else:
                 route_handler = routes[key].split("#", 1)
                 controller = getattr(importlib.import_module('app.controllers.'+route_handler[0]), route_handler[0])
                 _set_route(app, routes[key], key, controller, route_handler[1])
-
+                _routed_methods.append(routes[key])
+    """ Set up default routes based on controllers, their methods, and the method parameters """
+    for controller_name in controllers_list:
+        _controller_auto_router(app, controller_name, _routed_methods)
+""" Helper function that sets up routes for a given controller excluding all methods that are listed in "routed_methods" """
+def _controller_auto_router(app, controller_name, routed_methods):
+    controller = getattr(importlib.import_module('app.controllers.'+controller_name), controller_name)
+    methods = dict((x, inspect.getargspec(y).args) for x,y in controller.__dict__.items() if type(y) == FunctionType and not x.startswith('_'))
+    for method in methods:
+        parameters = '/'.join(['<'+param+'>' for param in methods[method] if param != 'self'])
+        if controller_name+'#'+method not in routed_methods:
+            if method != 'index':
+                _set_route(app, controller_name+'#'+method, '/'+controller_name.lower()+'/'+method+'/'+parameters, controller, method, methods=list(_verbs))
+            else:
+                _set_route(app, controller_name+'#'+method, '/'+controller_name.lower()+'/'+parameters, controller, method, methods=list(_verbs))
