40578cb709020ea3933ff71a9750fb5fb4beae8e
Ketul Patel <kjpatel2010@gmail.com>
2015-08-25 13:41:33 -0700

    disabled database connection

--- app/controllers/Welcome.py
+++ app/controllers/Welcome.py
@@ -15,8 +15,9 @@ class Welcome(Controller):
         """
             This is an example of loading a model.
             Every controller has access to the load_model method.
+
+            self.load_model('WelcomeModel')
         """
-        self.load_model('WelcomeModel')
 
     """ This is an example of a controller method that will load a view for the client """
     def index(self):
