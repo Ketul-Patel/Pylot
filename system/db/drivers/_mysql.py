f5e9ebd27781da1332505a917741cc09cf8f4066
oscarvazquez <oscar.vazquez2012@gmail.com>
2016-04-26 11:54:48 -0700

    clean up

--- system/db/drivers/_mysql.py
+++ system/db/drivers/_mysql.py
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
 import collections
 import inspect
-=======
-import mysql.connector
-import collections
-import inspect
-import models
->>>>>>> revisions setup
 from flask.ext.sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import text
 
@@ -20,11 +13,6 @@ def _convert(data):
     else:
         return data
 
-<<<<<<< HEAD
-=======
-
-
->>>>>>> revisions setup
 def connect(config, app):
     dbconfig = {
         'user': config.DB_USERNAME,
@@ -35,17 +23,10 @@ def connect(config, app):
     }
     dbconfig.update(config.DB_OPTIONS)
     app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://" + str(config.DB_USERNAME) + ":" + str(config.DB_PASSWORD) + "@127.0.0.1:" + str(config.DB_PORT) + "/" + config.DB_DATABASE_NAME
-<<<<<<< HEAD
-=======
-    for name, obj in inspect.getmembers(models):
-        if inspect.isclass(obj):
-            setattr(app, name, obj)
->>>>>>> revisions setup
     db = SQLAlchemy(app)
 
     def _query_db(query, data=None):
         result = db.session.execute(text(query), data)
-<<<<<<< HEAD
         if query[0:6].lower() == 'select':
             # if the query was a select
             # convert the result to a list of dictionaries
@@ -61,29 +42,11 @@ def connect(config, app):
         else:
             # if the query was an update or delete, return nothing and commit changes
             app.db.session.commit()
-=======
-        if query[0:6].lower() != 'select':
-            app.db.session.commit()
-            return True
-        else:
-            return result
->>>>>>> revisions setup
 
     def _get_one(query, data=None):
         result = db.session.execute(text(query), data).fetchone()
         return result
-<<<<<<< HEAD
         
     db.query_db = _query_db
     db.get_one = _get_one
     return db
-=======
-
-    db.query_db = _query_db
-    db.get_one = _get_one
-    return db
-
-
-
-
->>>>>>> revisions setup
