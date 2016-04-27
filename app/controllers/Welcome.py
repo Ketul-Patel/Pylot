1abee4184fafe67eefd9e7db0d142342042b4d1a
oscarvazquez <oscar.vazquez2012@gmail.com>
2016-04-27 13:20:13 -0700

    fixed unknown characters inside _mysql.py

--- app/controllers/Welcome.py
+++ app/controllers/Welcome.py
@@ -28,12 +28,12 @@ class Welcome(Controller):
         """
         A loaded model is accessible through the models attribute 
         self.models['WelcomeModel'].get_users()
-        
+
         self.models['WelcomeModel'].add_message()
-        # messages = self.models['WelcomeModel'].grab_messages()
-        # user = self.models['WelcomeModel'].get_user()
-        # to pass information on to a view it's the same as it was with Flask
+        messages = self.models['WelcomeModel'].grab_messages()
+        user = self.models['WelcomeModel'].get_user()
+        to pass information on to a view it's the same as it was with Flask
         
-        # return self.load_view('index.html', messages=messages, user=user)
+        return self.load_view('index.html', messages=messages, user=user)
         """
         return self.load_view('index.html')
