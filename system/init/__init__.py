52f61846f61d92d3d4067d449cfad9bf1ae39321
Mike Hannon <mjhannon@gmail.com>
2016-05-18 16:54:27 -0700

    update stable branch

--- system/init/__init__.py
+++ system/init/__init__.py
@@ -10,19 +10,17 @@ from app.config import routes
 from system.init.configuration import initialize_config
 from system.init.database import initialize_db
 from system.init.routes import initialize_routes
-from system.libraries.libraries import *
+
 
 def initialize_app():
 
     instance_path = os.path.abspath(os.path.dirname(__file__) + '/../..')
     template_folder = os.path.join(instance_path, 'app/views')
     static_folder = os.path.join(instance_path, 'app/static')
-
     app = Flask('app', static_folder=static_folder, template_folder=template_folder, instance_path=instance_path)
 
     initialize_config(app)
     initialize_db(app)
     initialize_routes(app)
-    initialize_libraries(app)
 
     return app
